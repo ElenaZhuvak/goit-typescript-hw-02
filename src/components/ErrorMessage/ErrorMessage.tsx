6827b2af8571efb2426e472672414fd868660049
Elena Zhuvak <elenkazh@gmail.com>
2025-03-12 22:02:53 +0000

    adds styles

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,9 +1,12 @@
-import { FC } from "react"
+import { FC } from 'react';
+import css from './ErrorMessage.module.css'
 
 const ErrorMessage: FC = () => {
   return (
+    <div className={css.errorMessage}>
       <p>Something went wrong</p>
-  )
-}
+    </div>
+  );
+};
 
-export default ErrorMessage
+export default ErrorMessage;
